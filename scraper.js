3796739d53d9e98e079074b8ec38afc2166d7f52
ryoichi-xl <jaownsatec@gmail.com>
2026-05-06 15:31:39 +0100

    chromiumSetUp

--- scraper.js
+++ scraper.js
@@ -13,7 +13,14 @@ async function scrapeImage(scrapeUrl) {
     const url = decodeURIComponent(scrapeUrl);
         const ext = [".png", ".jpg", ".jpeg", ".svg", ".webp"]
     try {
-        const browser = await puppeteer.launch( {headless: true});
+        const browser = await puppeteer.launch( {headless: true,
+            args: [
+    "--no-sandbox",
+    "--disable-setuid-sandbox",
+    "--disable-dev-shm-usage",
+    "--disable-gpu",
+        ],
+        });
         const page = await browser.newPage();
 
         await page.goto(url, { waitUntil: "domcontentloaded", timeout: 60000 });
